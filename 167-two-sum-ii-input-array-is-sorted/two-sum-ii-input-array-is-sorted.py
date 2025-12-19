8cb686b4b0282c26bfeee5f678bf11fb7fa104dd
natenate516 <natenate516@gmail.com>
2025-12-19 14:06:44 -0600

    Time: 7 ms (50.50%) | Memory: 13 MB (33.50%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+       l = 0
+       r = len(numbers) - 1
+       while l < r:
+        if numbers[l] + numbers[r] == target:
+            return [l+1,r+1]
+        elif numbers[l] + numbers[r] < target:
+            l = l + 1
+        else:
+            r = r - 1
