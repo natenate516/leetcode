ba87a7a110c73d438de93e6b5a581e599dd78696
natenate516 <natenate516@gmail.com>
2025-12-23 13:51:23 -0600

    Time: 55 ms (55.48%) | Memory: 27 MB (45.14%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        numSet = set(nums)
+        longest = 0
+
+        for num in numSet:
+            if (num - 1) not in numSet:
+                length = 1
+                while (num + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
