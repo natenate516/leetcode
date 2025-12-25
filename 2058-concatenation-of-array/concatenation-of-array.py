c6ff15261a4714b0545f25356cee0f69fc2ea3f2
natenate516 <natenate516@gmail.com>
2025-12-25 02:03:50 -0600

    Time: 4 ms (11.54%) | Memory: 12.8 MB (28.83%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        ans = []
+        for i, num in enumerate(nums):
+            ans.append(num)
+        for i, num in enumerate(nums):
+            ans.append(num)
+        return ans
