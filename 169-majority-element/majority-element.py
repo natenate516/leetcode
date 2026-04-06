95a59720fd00cea9fa1ddeb10289c4281a38881f
natenate516 <natenate516@gmail.com>
2026-04-06 14:44:45 -0500

    Time: 20 ms (7.30%) | Memory: 21.1 MB (92.66%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,9 +1,8 @@
-class Solution(object):
-    def majorityElement(self, nums):
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
         freq = {}
-        n = len(nums)
         for num in nums:
             freq[num] = freq.get(num, 0) + 1
-            if freq[num] > n // 2:
+            if freq[num] > (len(nums) // 2):
                 return num
         return -1
