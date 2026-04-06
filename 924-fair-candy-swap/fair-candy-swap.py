3e60400a762ffa59ab7e61a1a65cebeb58d45dbb
natenate516 <natenate516@gmail.com>
2026-04-06 15:44:25 -0500

    Time: 4629 ms (5.06%) | Memory: 21.4 MB (31.12%) - LeetSync

---
+++ 924-fair-candy-swap/fair-candy-swap.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
+        aCount = 0
+        bCount = 0
+        for candy in aliceSizes:
+            aCount += candy
+        for candy in bobSizes:
+            bCount += candy
+        
+        dif = (aCount - bCount) / 2
+
+        for candy in bobSizes:
+            if dif + candy in aliceSizes:
+                return [dif + candy, candy]
+        
