3e56c4b7d5acf37127c02ff168d7dfc2c6752541
natenate516 <natenate516@gmail.com>
2026-04-18 17:31:22 -0500

    Time: 12 ms (54.59%) | Memory: 19.3 MB (78.86%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,11 +1,12 @@
-class Solution(object):
-    def isAnagram(self, s, t):
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        
         countS = {}
         countT = {}
-        for index in range(len(s)):
-            countS[s[index]] = countS.get(s[index],0) + 1
-            countT[t[index]] = countT.get(t[index],0) + 1
-        return countS == countT
+        for i in range(len(s)):
+            countS[s[i]] = countS.get(s[i], 0) + 1
+            countT[t[i]] = countT.get(t[i], 0) + 1
+
+        return countT == countS
+        
