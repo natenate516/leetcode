d22736756ef3671ee67802fcf19b11ee553e4b86
natenate516 <natenate516@gmail.com>
2026-04-19 14:24:26 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (20.97%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
@@ -1,11 +1,11 @@
-class Solution(object):
-    def maxDepth(self, s):
-        currCount = 0
-        maxCount = 0 
-        for char in s:
-            if char == '(':
-                currCount += 1
-                maxCount = max(maxCount, currCount)
-            elif char == ')':
-                currCount -= 1
-        return maxCount
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        openP = 0
+        maxOpen = 0
+        for c in s:
+            if c == "(":
+                openP += 1
+            if c == ")":
+                openP -= 1
+            maxOpen = max(openP,maxOpen)
+        return maxOpen
